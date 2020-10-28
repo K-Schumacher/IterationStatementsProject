7ae39d3f23713a734c01d7c214fd16a7d14981b5
Kyle Scumacher <kschumacher001@gmail.com>
2020-10-28 16:41:24 -0400

    completed exercise

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -10,36 +10,44 @@ namespace IterationStatements
             //TODO
 
             //Create a List called "numbers" - DONE
-            var numbers = new List<int>();
-            
-            //Create a variable of type int and initialize the variable with a value of 0
+            List<int> numbers = new List<int>();
 
+            //Create a variable of type int and initialize the variable with a value of 0
+            int numList = 0;
 
 
             // Create a do-while loop
+            do
             {
                 // Increment your variable by 1
-
+                numList++;
                 // Then add your variable to "numbers"
-                numbers.Add(/* variable name goes here */);
-            }
+                numbers.Add(numList);
+            } while (numList < 100);
             // While your variable is less than 100
 
 
 
             // Create a while loop
             // While your variable is less than 200
+            while (numList < 200)
             {
+                numList++;
                 // Increment your variable by 1
                 // Then add your variable to "numbers"
+                numbers.Add(numList);
             }
 
             Console.WriteLine("Increase:");
 
             // Create a foreach loop
             // Write your variable to the console
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number}");
+            }
 
-            Console.WriteLine("");
+            Console.WriteLine($"************");
             Console.WriteLine("Decrease:");
 
             // Create a for loop
@@ -47,7 +55,13 @@ namespace IterationStatements
             // in your conditional, as long as i is less than or equal to the amount of items in "numbers" (numbers.Count)
             // and as long as i is greater than or equal to 0
             // Decrement i by 1
+            for (int i = 199; i <= numbers.Count; i--)
             {
+                if (i >= 0)
+                {
+                    Console.WriteLine($"{i}");
+                }
+                
                 // Write to the console "numbers" at index i
             }
         }
